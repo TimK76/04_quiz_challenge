01bb819dfad3cccaeebeaed3b6eebaf5dd83706c
https://uconn.bootcampcontent.com/theknightmoores-xJPRH <theknightmoores@gmail.com>
2022-02-19 13:18:01 -0500

    Got timer working thanks to Jaime!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,31 @@
 // Build the Timer countdown function.
-var countdown = setInterval(timer, 1000);
-function quizStart() {
-    if(quizStart) timer--;
-    if(timer <=0) {
-        quizStop();
-        timer = 75*1000;
+var countdown;
+var time = 75;
+// var quizStart = document.getElementById("timer");
+var quizStop;
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start");
+function timer() {
+    time --;
+    console.log(time);
+    timerEl.innerHTML = "Remaining Time: " + time;
+    if(time <=0) {
+        clearInterval(countdown);
+    }else if(questions <=0){
+        // tell the quiz to proceed to the end of the quiz and display score
+        document.getElementById("quizFinished");
     }
-        timerEl.innerHTML = "Remaining Time: " + timer;
-        console.log(timer)
-    }  
+}
+
+function quizStart() {
+    countdown = setInterval(timer, 1000);
+    // if(quizStart) timer--;
+    //     quizStop();
+    // }
+    //     console.log(timer)
+}  
+
 // Tell the timer to start when the "Start Quiz" button is pressed and to continue running until the end of the quiz.    
-addEventListener("click", quizStart);
+startButton.addEventListener("click", quizStart);
 
-
-
- 
 //tell the timer to stop either when all quiz questions are answered or when the timer reaches 0
-clearInterval(quizStop)
